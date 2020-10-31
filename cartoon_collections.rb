563c04e2896db02915c092df50f00bf196a105e6
Mark G <markghaida@gmail.com>
2020-10-31 01:54:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,9 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
   
-  array.each do |
+  array.each do |greeting|
+    puts "Hello #{greeting}!"
+  end
 end
 
 def list_dwarves(array)
