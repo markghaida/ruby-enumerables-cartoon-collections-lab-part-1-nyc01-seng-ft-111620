3616ff413d77914ea85176bed37897909a267659
Mark G <markghaida@gmail.com>
2020-10-31 01:57:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |array, index|
-    puts "#{index}. #{array}"
+    puts "#{index+1}. #{array}"
     
   end
   
